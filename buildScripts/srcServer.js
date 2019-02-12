d030520f3bc1efed256b92288d086fa7b1c84797
Mike Bova <bova_m@subway.com>
2019-02-12 13:29:37 -0500

    Http Calls before Mocking

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,9 +21,9 @@ app.get('/', function (req, res) {
 
 app.get('/users', function (req, res) {
   res.json([
-    {"id":1, "firstName":"Bob", "LastName":"Smith", "email":"bob@gmail.com"},
-    {"id":2, "firstName":"Tammy", "LastName":"Norton", "email":"tnorton@gmail.com"},
-    {"id":3, "firstName":"Tina", "LastName":"Lee", "email":"lee.tina@gmail.com"},
+    {"id":1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id":2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@gmail.com"},
+    {"id":3, "firstName":"Tina", "lastName":"Lee", "email":"lee.tina@gmail.com"},
   ]);
 });
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+import {getUsers} from './api/userApi';
 //import './index.css'
 //import numeral from 'numeral';
 
 //const courseValue = numeral(1000).format('$0,0.00');
 //console.log(`I would pay ${courseValue} for this awesome course!`);
+
+getUsers().then(result => {
+  let userBody = '';
+
+  result.forEach(user => {
+    userBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUseer">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = userBody;
+});
